72321bb5a6f60eb141cbe0b0ed400fe32ba4ba1a
Daniel Lopez <dlopezre95@gmail.com>
2021-11-27 09:04:30 +0100

    done

--- app.js
+++ app.js
@@ -21,5 +21,24 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.get('/', (req, res) => {
   res.render('index');
 });
+app.get('/beers', (req, res) => {
+  punkAPI.getBeers()
+  .then(beersFromApi => {
+    console.log('beers from the database', beersFromApi)
+    res.render('beers', { beersFromApi })
+  })
+  .catch(error => console.log(error));
+});
+
+app.get('/random-beer', (req, res) => {
+  punkAPI.getRandom()
+  .then(responeFromApi => {
+    console.log('random one?', responeFromApi)
+    res.render('random-beer', {responeFromApi})
+  })
+  .catch(error => console.log(error));
+});
+
+
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
